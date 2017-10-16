f19181ed09823e8c0a8a6cf7980bdccc31ec5b9b
apple <apple@apples-MacBook-Pro.local>
2017-10-16 18:47:10 +0530

    First and last commit

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
 import random, pandas as pd
-print(pd.read_excel('films.xls')['Title of the Film'].tolist()[random.randint(0,len(pd.read_excel('films.xls'))-1)])
+print(pd.read_excel('films.xls')['Title of the Film'].tolist()[random.randint(0, 1000)])
